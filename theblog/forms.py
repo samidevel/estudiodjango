15de1d2c01d01d67772e12041cd014723b9aa173
samidevel <yesideveloper@gmail.com>
2022-11-10 21:19:35 -0500

    primer commint de blog completo

---
+++ theblog/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Post, Category
+
+
+#choices = [('fushing'),('python'),('animales')]
+choices = Category.objects.all().values_list('name','name')
+choices_list = []
+for item in choices:
+	choices_list.append(item)
+
+
+class PostForm(forms.ModelForm):
+	class Meta:
+
+		model  = Post
+		fields = ('title', 'title_tag', 'author', 'category', 'body')
+
+		widgets ={
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			'title_tag': forms.TextInput(attrs={'class': 'form-control' }),
+			'author': forms.Select(attrs={'class': 'form-control' }),
+			'category': forms.Select(choices=choices_list, attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control' }),
+
+		}
+
+
+
+
+class EditForm(forms.ModelForm):
+	class Meta:
+
+		model  = Post
+		fields = ('title', 'title_tag', 'body')
+
+		widgets ={
+			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Este es un placeholder'}),
+			'title_tag': forms.TextInput(attrs={'class': 'form-control' }),
+			#'author': forms.Select(attrs={'class': 'form-control' }),
+			'body': forms.Textarea(attrs={'class': 'form-control' }),
+
+		}
